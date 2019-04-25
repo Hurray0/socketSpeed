2bb8d0a17e7c34c61df690ce662b7963b72deaaf
Ubuntu <ubuntu@ip-172-31-27-225.ap-northeast-1.compute.internal>
2019-04-25 08:33:08 +0000

    [sync] tcp client

--- TCP/client.cpp
+++ TCP/client.cpp
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <iostream>
 #include <unistd.h>
 #include <sys/types.h>
 #include <sys/socket.h>
@@ -9,8 +10,9 @@
 #include <time.h>
 
 #define SERV_PORT	39407
-#define BUF_SIZE	65000
+#define BUF_SIZE	1000000
 
+using namespace std;
 
 int main(int argc , char *argv[])
 {
@@ -47,17 +49,20 @@ int main(int argc , char *argv[])
     //char receiveMessage[100] = {};
     char *send_buf;
     send_buf = (char *)malloc(BUF_SIZE*sizeof(char));
+    cout << sizeof(char) * BUF_SIZE << endl;
     for (int j = 0; j < REPEAT_M; j++) {
 	    clock_gettime(CLOCK_REALTIME, &tn);
 	    for (int i = 0; i < REPEAT_N; i++) {
-		    send(sockfd, send_buf,sizeof(send_buf),0);
+		    send(sockfd, send_buf, BUF_SIZE*sizeof(char),0);
 	    }
+
 	    clock_gettime(CLOCK_REALTIME, &tn2);
 	    double start_time = tn.tv_sec * 1000000000 + tn.tv_nsec;
 	    double end_time = tn2.tv_sec * 1000000000 + tn2.tv_nsec;
 	    double diff = end_time - start_time;
 	    double throughput = REPEAT_N*BUF_SIZE*sizeof(char)/diff*8/1.024/1.024/1.024;
-	    printf("time = %lfns, throughput = %lfGbps\n", diff, throughput);
+	    //printf("time = %lfns, buf = %dbits, throughput = %lfGbps\n", diff, sizeof(send_buf), throughput);
+	    cout << "time = " << diff << "ns, buf = " << REPEAT_N*BUF_SIZE*sizeof(char) << "bits, throughput = " << throughput << "Gbps" << endl;
     }
     //recv(sockfd,receiveMessage,sizeof(receiveMessage),0);
 
